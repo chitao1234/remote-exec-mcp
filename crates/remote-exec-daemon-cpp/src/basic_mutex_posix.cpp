03a2b7ae726073d6461e846862488d14646e2f94
Xuntao Chi <chotaotao1qaz2wsx@gmail.com>
2026-05-19 01:03:36 +0800

    Probe pthread condvar clock support on POSIX

--- crates/remote-exec-daemon-cpp/src/basic_mutex_posix.cpp
+++ crates/remote-exec-daemon-cpp/src/basic_mutex_posix.cpp
@@ -2,6 +2,10 @@
 
 #include <ctime>
 
+#if defined(CLOCK_MONOTONIC) && defined(__GNUC__) && !defined(__APPLE__)
+extern "C" int pthread_condattr_setclock(pthread_condattr_t* attr, clockid_t clock_id) __attribute__((weak));
+#endif
+
 BasicMutex::BasicMutex() {
     pthread_mutex_init(&mutex_, nullptr);
 }
@@ -20,11 +24,23 @@ void BasicMutex::unlock() {
 
 namespace {
 
+bool pthread_condattr_setclock_available() {
+#if defined(CLOCK_MONOTONIC) && defined(__GNUC__) && !defined(__APPLE__)
+    return pthread_condattr_setclock != nullptr;
+#else
+    return false;
+#endif
+}
+
 bool init_condvar_monotonic(pthread_cond_t* cond) {
-#if defined(__APPLE__)
+#if !defined(CLOCK_MONOTONIC)
     pthread_cond_init(cond, nullptr);
     return false;
 #else
+    if (!pthread_condattr_setclock_available()) {
+        pthread_cond_init(cond, nullptr);
+        return false;
+    }
     pthread_condattr_t attr;
     if (pthread_condattr_init(&attr) != 0) {
         pthread_cond_init(cond, nullptr);
@@ -41,6 +57,17 @@ bool init_condvar_monotonic(pthread_cond_t* cond) {
 #endif
 }
 
+clockid_t condvar_deadline_clock(bool uses_monotonic) {
+#ifdef CLOCK_MONOTONIC
+    if (uses_monotonic) {
+        return CLOCK_MONOTONIC;
+    }
+#else
+    (void)uses_monotonic;
+#endif
+    return CLOCK_REALTIME;
+}
+
 } // namespace
 
 BasicCondVar::BasicCondVar() : uses_monotonic_(init_condvar_monotonic(&cond_)) {}
@@ -55,7 +82,7 @@ void BasicCondVar::wait(BasicMutex& mutex) {
 
 bool BasicCondVar::timed_wait_ms(BasicMutex& mutex, unsigned long timeout_ms) {
     struct timespec deadline;
-    clock_gettime(uses_monotonic_ ? CLOCK_MONOTONIC : CLOCK_REALTIME, &deadline);
+    clock_gettime(condvar_deadline_clock(uses_monotonic_), &deadline);
     deadline.tv_sec += static_cast<time_t>(timeout_ms / 1000UL);
     deadline.tv_nsec += static_cast<long>((timeout_ms % 1000UL) * 1000000UL);
     if (deadline.tv_nsec >= 1000000000L) {
